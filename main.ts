d1f98fa92accbfd9f4337ffd8a4773c180dbf874
AKMnSCAR <115064941+AKMnSCAR@users.noreply.github.com>
2022-10-05 14:48:02 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+	
+})
